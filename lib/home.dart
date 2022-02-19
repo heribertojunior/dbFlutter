533366a86041a90c6ebb7e7a51c3b0c5fbd87875
heribertojunior <hsc.jr94@gmail.com>
2022-02-19 14:50:00 -0400

    vai meu fio

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  _recuperarBandoDados() async {
+  _recuperarBancoDados() async {
     final caminhoBancoDados = await getDatabasesPath();
     final localBancoDados = join(caminhoBancoDados, "banco.db");
 
@@ -20,15 +20,36 @@ class _HomeState extends State<Home> {
       onCreate: (db, dbversion) {
         //criação do bd
         String sql =
-            "CREATE TABLE usuarios(idusuario INTEGER PRUMARY KEY AUTOINCREMENT, nome VARCHAR,idade INTEGER)";
+            "CREATE TABLE usuarios (idusuario INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR,idade INTEGER)";
         db.execute(sql);
       },
     );
-    print("aberto: " + retorno.isOpen.toString());
+    //print("aberto: " + retorno.toString());
+    return retorno;
+  }
+
+  _salvar() async {
+    Map<String, dynamic> dadosUser = {"nome": "Jacaina Moita", "idade": 15};
+    Database dbs = await _recuperarBancoDados();
+    int id = await dbs.insert("usuarios", dadosUser);
+    print("salvo: " + id.toString());
+  }
+
+  _listar() async {
+    Database dbs = await _recuperarBancoDados();
+    String sql = "SELECT * FROM usuarios";
+    List usuariosLista = await dbs.rawQuery(sql);
+
+    for (var usuario in usuariosLista) {
+      print("Id : " + usuario['idusuario'].toString());
+      print("Nome : " + usuario['nome']);
+      print("Idade : " + usuario['idade'].toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    _listar();
+    return Scaffold();
   }
 }
